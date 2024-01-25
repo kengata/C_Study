bacbea7bc84ad40770299040795327d509295d85
kenichiro yamagata <kenichiro.yamagata@gmail.com>
2024-01-25 23:07:15 +0900

    first commit

---
+++ C_Study.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : C_Study.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("横道世之介"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
